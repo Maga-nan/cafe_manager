178b295d963a18ff6ea933908a7b3e12902e4449
pashtet <pavelpogreban22@gmail.com>
2026-03-17 18:10:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,3 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///cafe.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    # 🔥 НАСТРОЙКИ ПОЧТЫ (Gmail)
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = 'pavelpogreban22@gmail.com'  # Твой Gmail
-    MAIL_PASSWORD = 'nulo lbnt fkol gvdi'        # Твой пароль приложения
-    MAIL_DEFAULT_SENDER = 'pavelpogreban22@gmail.com'
